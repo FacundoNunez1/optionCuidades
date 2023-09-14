0239853a31accf5285a8709944b1da91ef258c95
FacundoNunez1 <fakundo1213@gmail.com>
2023-09-13 21:24:39 -0300

    facture 1

--- src/App.js
+++ src/App.js
@@ -8,13 +8,30 @@ function App() {
   const [centro, setCentro] = useState(["centro1", "centro2", "centro3"])
   const [seleccion, setSelecion] = useState("");
   const [subSeleccionC, setSubSelecion] = useState("");
+  const [estado,setEstado] = useStatetate(false);
+
+useEffect({
+
+
+
+
+},[])
+const cardMontevido = () =>{
+
+return <div>
+
+  <p>Montevideo</p>
+</div>
+
+
+}
 
   return (
     <div className="App">
 
       <select >
         {cuidades.map((item) => {
-          return <option key={item}> {item}</option>
+          return <option key={item} > {item}</option>
         })}
     
       </select>
